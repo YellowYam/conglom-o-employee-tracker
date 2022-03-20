5ccd6abe0e0f8ad0f1e38e015b5f5f9e3c423cd2
Cody S <cody.scoles@gmail.com>
2022-03-20 16:55:51 -0400

    Database written

---
+++ db/seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (department_name)
+VALUES ("Peripherals"),
+       ("Components "),
+       ("Coffee Shop"),
+       ("Computers");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Customer Service", 10000, 1),
+       ("Software Developer", 70000, 4),
+       ("Artist", 70000, 4),
+       ("Barista", 14000, 3),
+       ("Manager", 100000, 4);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Elliot", "Smith", 2 , NULL),
+       ("Amira", "Afzal", 1, 1),
+       ("Christoper", "Lee", 3, 1),
+       ("Verónica", "Rodriguez", 4, 1),
+       ("Igor", "Ivanov", 2, 1);
--- index.js
+++ index.js
@@ -1,16 +1,144 @@
- const employeeManagerLogo = 
-   ",------------------------------------------------------------------------------------------------------------------------.\n"
-  + "|  _____                       _                                   __  __                                                 |\n" 
+// Import inquirer module
+const inquirer = require('inquirer');
+// Import mysql2
+const mysql = require('mysql2');
+
+/** Inquirer questions to select new employee type **/
+const mainMenu = {
+  type: 'list',
+  name: 'menu_choice',
+  message: 'Main Menu: ',
+  choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role',
+    'add an employee', 'update an employee role'],
+};
+const employeeManagerLogo =
+  ",------------------------------------------------------------------------------------------------------------------------.\n"
+  + "|  _____                       _                                   __  __                                                 |\n"
   + "|  | ____|  _ __ ___    _ __   | |   ___    _   _    ___    ___    |  \\/  |   __ _   _ __     __ _    __ _    ___   _ __  |\n"
   + "|  |  _|   | '_ ` _ \\  | '_ \\  | |  / _ \\  | | | |  / _ \\  / _ \\   | |\\/| |  / _` | | '_ \\   / _` |  / _` |  / _ \\ | '__| |\n"
   + "|  | |___  | | | | | | | |_) | | | | (_) | | |_| | |  __/ |  __/   | |  | | | (_| | | | | | | (_| | | (_| | |  __/ | |    |\n"
   + "|  |_____| |_| |_| |_| | .__/  |_|  \\___/   \\__, |  \\___|  \\___|   |_|  |_|  \\__,_| |_| |_|  \\__,_|  \\__, |  \\___| |_|    |\n"
   + "|                      |_|                  |___/                                                    |___/                |\n"
-  + "`-------------------------------------------------------------------------------------------------------------------------'"      
-  
-  function init(){
-      console.log(employeeManagerLogo);
-  }
+  + "`-------------------------------------------------------------------------------------------------------------------------'"
 
-  init();
-                                                                                               
+
+function connectToDB(database_name, database_password){
+  // Connect to database
+  const mysql = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: database_password,
+      database: database_name
+    },
+    console.log(`Connected to the {database} database.`)
+  );
+
+}
+function init() {
+  //Begin inputs
+  inquirer
+    //Load main menu
+    .prompt(mainMenu)
+    .then((data) => {
+      switch (data.menu_choice) {
+        case "view all departments":
+          connectToDB()
+          
+          break;
+        case "view all roles":
+           // Connect to database
+           const viewAllRolesConnection = viewAllRolesConnection.createConnection(
+            {
+              host: 'localhost',
+              // MySQL username,
+              user: 'root',
+              // MySQL password
+              password: 'IntacHATLCha30&!@',
+              database: ''
+            },
+            console.log(`Connected to the {database} database.`)
+          );
+          break;
+        case "view all employees":
+           // Connect to database
+           const viewAllEmployeesConnection = viewAllRolesConnection.createConnection(
+            {
+              host: 'localhost',
+              // MySQL username,
+              user: 'root',
+              // MySQL password
+              password: '',
+              database: ''
+            },
+            console.log(`Connected to the {database} database.`)
+          );
+          break;
+        case "add a department":
+           // Connect to database
+           const addADepartmentConnection = addADepartmentConnection.createConnection(
+            {
+              host: 'localhost',
+              // MySQL username,
+              user: 'root',
+              // MySQL password
+              password: '',
+              database: ''
+            },
+            console.log(`Connected to the {database} database.`)
+          );
+          break;
+        case "add a role":
+           // Connect to database
+           const addARoleConnection = addARoleConnection.createConnection(
+            {
+              host: 'localhost',
+              // MySQL username,
+              user: 'root',
+              // MySQL password
+              password: '',
+              database: ''
+            },
+            console.log(`Connected to the {database} database.`)
+          );
+          break;
+        case "add an employee":
+           // Connect to database
+           const addAnEmployeeConnection = addAnEmployeeConnection.createConnection(
+            {
+              host: 'localhost',
+              // MySQL username,
+              user: 'root',
+              // MySQL password
+              password: '',
+              database: ''
+            },
+            console.log(`Connected to the {database} database.`)
+          );
+          break;
+        case "update an employee role":
+           // Connect to database
+           const updateAnEmployeeRoleConnection = updateAnEmployeeRoleConnection.createConnection(
+            {
+              host: 'localhost',
+              // MySQL username,
+              user: 'root',
+              // MySQL password
+              password: '',
+              database: ''
+            },
+            console.log(`Connected to the {database} database.`)
+          );
+          break;
+        default:
+          console.error("No cases found");
+          break;
+      }
+    })
+    .catch((err) => console.error(err));
+}
+  
+
+init();
