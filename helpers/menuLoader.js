c697afe83c06f5905b6f5f15fa58cc0948b8841b
Cody S <cody.scoles@gmail.com>
2022-03-20 22:20:45 -0400

    Add role function to sqlUtilities

--- helpers/menuLoader.js
+++ helpers/menuLoader.js
@@ -1,38 +1,63 @@
 // Import inquirer module
 const inquirer = require('inquirer');
 
-/** Inquirer questions to select new employee type **/
-const mainMenu = {
-    type: 'list',
-    name: 'menu_choice',
-    message: 'Main Menu: ',
-    choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role',
-      'add an employee', 'update an employee role'],
-  };
+function loadMainMenu() {
 
-  /** Inquirer questions to create a new department **/
-const departmentCreator = {
-    type: 'input',
-    name: 'department_name',
-    message: 'What shall the department be named?',
-  };
-
-  
-
-
-  function loadMainMenu(){
+    /** Inquirer questions to select new employee type **/
+    const mainMenu = {
+        type: 'list',
+        name: 'menu_choice',
+        message: 'Main Menu: ',
+        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role',
+            'add an employee', 'update an employee role'],
+    };
 
     return inquirer
-      //Load main menu
-      .prompt(mainMenu);
-  }
+        //Load main menu
+        .prompt(mainMenu);
+}
 
-  function loadDepartmentCreator(){
+
+function loadDepartmentCreator() {
+
+    /** Inquirer questions to create a new department **/
+    const departmentCreator = [
+        {
+            type: 'input',
+            name: 'department_name',
+            message: 'What shall the department be named?',
+        },
+    ];
 
     return inquirer
         .prompt(departmentCreator);
-  }
-  
+}
+
+function loadRoleCreator(roles) {
+
+    /** Inquirer questions to create a new role **/
+    const roleCreator = [
+        {
+            type: 'input',
+            name: 'role_name',
+            message: 'What shall the role be named?',
+        },
+        {
+            type: 'number',
+            name: 'salary',
+            message: 'What shall the role be paid?',
+        },
+        {
+            type: 'list',
+            name: 'department',
+            message: 'To which department shall this role belong?',
+            choices: roles
+        },
+    ];
+
+    return inquirer
+        .prompt(roleCreator);
+}
 
 
-  module.exports = {loadMainMenu, loadDepartmentCreator};
+module.exports = { loadMainMenu, loadDepartmentCreator, loadRoleCreator };
--- helpers/sqlUtilities.js
+++ helpers/sqlUtilities.js
@@ -1,13 +1,13 @@
 // Import mysql2
 const mysql = require('mysql2');
 // Import menu loader
-const {loadMainMenu, loadDepartmentCreator} = require('./menuLoader');
+const { loadMainMenu, loadDepartmentCreator, loadRoleCreator } = require('./menuLoader');
 //MySQL pwd
 const pwd = "IntacHATLCha30&!@";
 //Employee Manager Database
 const employee_db = "employee_db";
 
-function connectToDB(database_name, database_password){
+function connectToDB(database_name, database_password) {
   // Connect to database
   const db = mysql.createConnection(
     {
@@ -26,36 +26,44 @@ function connectToDB(database_name, database_password){
 }
 
 
-function viewTable(table_name, connection){
-     // Queries the database to display all the contents of the departments table
-     connection.promise().query(`SELECT * FROM ${table_name}`)
-        .then(([rows]) => {
-            console.table(rows);
-            loadMainMenu()
-            .then((data) => {processMenuSelection(data)})
-            .catch(err => console.error(err));
-      })
-        .catch((err) => console.error(err));
+function viewTable(table_name, connection) {
+  // Queries the database to display all the contents of the departments table
+  connection.promise().query(`SELECT * FROM ${table_name}`)
+    .then(([rows]) => {
+      console.table(rows);
+      loadMainMenu()
+        .then((data) => { processMenuSelection(data) })
+        .catch(err => console.error(err));
+    })
+    .catch((err) => console.error(err));
 }
 
-function addDepartment(department_name, connection){
-   // Queries the database to display all the contents of the departments table
-   connection.promise().query(`INSERT INTO department (department_name) VALUES("${department_name}")`)
-   .then(([rows]) => {
-       console.table(rows);
-       loadMainMenu()
-       .then((data) => {processMenuSelection(data)})
-       .catch(err => console.error(err));
- })
-   .catch((err) => console.error(err));
-    
-}
-
-function addRole(){
+function addDepartment(department_name, connection) {
+  // Queries the database to add a department
+  connection.promise().query(`INSERT INTO department (department_name) VALUES("${department_name}")`)
+    .then(([rows]) => {
+      console.table(rows);
+      loadMainMenu()
+        .then((data) => { processMenuSelection(data) })
+        .catch(err => console.error(err));
+    })
+    .catch((err) => console.error(err));
 
 }
 
-function addEmployee(){
+function addRole(role_name, salary, department_id, connection) {
+  // Queries the database to add a role
+  connection.promise().query(`INSERT INTO role (title, salary, department_id) VALUES("${role_name}", ${salary}, ${department_id})`)
+    .then(([rows]) => {
+      console.table(rows);
+      loadMainMenu()
+        .then((data) => { processMenuSelection(data) })
+        .catch(err => console.error(err));
+    })
+    .catch((err) => console.error(err));
+}
+
+function addEmployee() {
 
 
 }
@@ -67,48 +75,74 @@ function processMenuSelection(data) {
       // Connects to database
       const viewAllDepartmentsConnection = connectToDB(employee_db, pwd);
 
-       // Queries the database to display all the contents of the departments table
+      // Queries the database to display all the contents of the departments table
       viewTable('department', viewAllDepartmentsConnection);
-      
+
 
 
       //End case
       break;
     case "view all roles":
-       // Connect to database
-       const viewAllRolesConnection = connectToDB(employee_db, pwd);
+      // Connect to database
+      const viewAllRolesConnection = connectToDB(employee_db, pwd);
 
-       // Queries the database to display all the contents of the roles table
-       viewTable('role', viewAllRolesConnection);
+      // Queries the database to display all the contents of the roles table
+      viewTable('role', viewAllRolesConnection);
 
-       
-       //End case
+
+      //End case
       break;
     case "view all employees":
-       // Connect to database
-       const viewAllEmployeesConnection = connectToDB(employee_db, pwd);
+      // Connect to database
+      const viewAllEmployeesConnection = connectToDB(employee_db, pwd);
 
-       // Queries the database to display all the contents of the employees table
-       viewTable('employee', viewAllEmployeesConnection);
-       
-       //End case
+      // Queries the database to display all the contents of the employees table
+      viewTable('employee', viewAllEmployeesConnection);
+
+      //End case
       break;
     case "add a department":
-       // Connect to database
-       const addADepartmentConnection = connectToDB(employee_db, pwd);
-       loadDepartmentCreator()
-       .then((data) => {addDepartment(data.department_name, addADepartmentConnection)})
-       .catch(err => console.error(err));
-       
+      // Connect to database
+      const addADepartmentConnection = connectToDB(employee_db, pwd);
+      loadDepartmentCreator()
+        .then((data) => { addDepartment(data.department_name, addADepartmentConnection) })
+        .catch(err => console.error(err));
+
 
       break;
     case "add a role":
-       // Connect to database
-       const addARoleConnection = connectToDB(employee_db, pwd);
-      break;
+      // Connect to database
+      const addARoleConnection = connectToDB(employee_db, pwd);
+
+      addARoleConnection.promise().query('SELECT title, department_id FROM role')
+        .then(([rows]) => {
+         const roles = []; 
+         for(let i = 0; i < rows.length ; i++){
+            roles.push(rows[i].title);
+         }
+         
+         
+          loadRoleCreator(roles, rows)
+            .then((data) => { 
+            
+              // A filter function to retrieve the id for the ammended department
+              function findDepartment(row){
+                return row.title === data.department;
+              }
+
+              const department = rows.filter(findDepartment)[0].department_id;
+
+            
+            
+              addRole(data.role_name, data.salary, department, addARoleConnection) })
+            .catch(err => console.error(err));
+        })
+        .catch(err => console.error(err));
+
+          break;
     case "add an employee":
-       // Connect to database
-       const addAnEmployeeConnection= connectToDB(employee_db, pwd);
+      // Connect to database
+      const addAnEmployeeConnection = connectToDB(employee_db, pwd);
       break;
     default:
       console.error("No cases found");
@@ -116,4 +150,4 @@ function processMenuSelection(data) {
   }
 }
 
-module.exports = {connectToDB, viewTable, addDepartment, processMenuSelection};
+module.exports = { connectToDB, viewTable, addDepartment, processMenuSelection };
